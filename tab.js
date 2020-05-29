d9862bbbb6eae1c51d4634547d76f2ca518d2f71
Seoryeong Oh <osr0423@naver.com>
2020-05-29 20:42:11 +0900

    clock

---
+++ tab.js
@@ -0,0 +1,15 @@
+const selectClock = document.querySelector('#selectClock');
+const selectStopwatch = document.querySelector('#selectStopwatch');
+const w_clock = document.querySelector('#w_clock');
+const w_stopwatch = document.querySelector('#w_stopwatch');
+
+selectClock.onclick = function(){
+    w_clock.style.display = 'block';
+    w_stopwatch.style.display = 'none';
+    w_timer.style.display = 'none';
+}
+selectStopwatch.onclick = function(){
+    w_clock.style.display = 'none';
+    w_stopwatch.style.display = 'block';
+    w_timer.style.display = 'none';
+}
